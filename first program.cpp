9015bc4e654f3eb89f8b64fd84aa8bdabe1c4a78
Lijala Shakya <lijalashakya01@gmail.com>
2023-05-17 21:29:13 +0545

    firstcommit

--- first program.cpp
+++ first program.cpp
@@ -1,24 +1,33 @@
 #include<iostream>
 using namespace std;
 class student{
-    char address [40];
+    char assignment [40];;
     int rollnumber;
+public:
 
+  void read(){
+    cout<<"Enter rollnumber:";
+    cin>>rollnumber;
+  }
 
-void assignment(){
-
-    cout << "completed" <<endl;
-};
-void exam(){
-
-    cout << "passed" <<endl;
-};
+  void assignment(){
+    
+    cout << "assignment submission [Y/N]" ;
+    cin>>assignment;
+  }
+  void info(){
+    cout<<"rollnumber :"<<rollnumber;
+    cout << "assignment submission:" <<assignment<<;
+  }
 };
 
 int main()
 {
-    student lijala, sanjina ;
-    cout << "sanjina".rollnumber();
-    cout << "sanjina".assignment();
+    student sanjina ;
+    cout << "student: sanjina"<<endl;
+    sanjina.read();
+    sanjina.info();
+    sanjina.assignment();
 
-};
+
+}
--- firstclass.cpp
+++ firstclass.cpp
@@ -6,24 +6,27 @@ class Room{
     int breadth;
     int height;
 
-    double calculateArea() {
-        return length * breadth ;
+    void info(){
+        cout<<"Enter length, breadth, height:";
+        cin>>length>>breadth>>height;
     }
 
-    double calculateVolume(){
-        return length * breadth * height ;
-    }
+    void calculateArea() {
+       cout<<"The area of the room is :"<<length*breadth;
 
+    }
+    void calculateVolume(){
+        cout<<"The volume of the room is :"<<length*breadth*height;
+    }
 };
 
 int main(){
     Room obj401;
-    obj401.length = 10;
-    obj401.breadth=5;
-    obj401.height=5;
-
-    cout<<"The area of the room is:"<<obj401.calculateArea();
-    cout<<"The volume of the room is:"<<obj401.calculateVolume();
+    obj401.info();
+    obj401.calculateArea();
+    obj401.calculateVolume();
+    
+    
     
 
 }
